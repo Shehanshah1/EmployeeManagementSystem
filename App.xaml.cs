5e11ee1eadfefc4df3e57f0f2cf85dfd7d156855
Shehanshah1 <MuhammadAsjad.RehmanHashmi@gmail.com>
2024-10-29 18:58:54 -0500

    Login Page Designed

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,13 @@
-﻿namespace EmployeeManagementSystem
+﻿using EmployeeManagementSystem.Vieww;
+
+namespace EmployeeManagementSystem
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new LoginView();
         }
     }
 }
---
+++ Vieww/LoginView.xaml.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagementSystem.Vieww;
+
+public partial class LoginView : ContentPage
+{
+	public LoginView()
+	{
+        InitializeComponent();
+    }
+}
