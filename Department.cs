fceb70fe3741b2215085c8c1fc680d21fafef69b
RehamAfzal <reham.afzal1@gmail.com>
2024-11-10 17:55:04 -0600

    Update Department.cs

--- Department.cs
+++ Department.cs
@@ -1,1 +1,9 @@
+namespace EmployeeManagementSystem
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public required string DepartmentName { get; set; }
+    }
+}
 
