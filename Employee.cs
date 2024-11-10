e2310ef92e9339094f9dc919e1d40f03dde164e8
RehamAfzal <reham.afzal1@gmail.com>
2024-11-10 17:55:18 -0600

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,1 +1,14 @@
+using System;
+
+namespace EmployeeManagementSystem
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public int DepartmentID { get; set; }
+        public string Position { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
 
