e486383a0c8961fc553e1a3d684bb4cb115989c8
rondickson <146130094+rondickson@users.noreply.github.com>
2024-11-18 16:25:27 -0600

    Update Dashboard.xaml.cs

--- Views/Dashboard.xaml.cs
+++ Views/Dashboard.xaml.cs
@@ -4,19 +4,44 @@ using EmployeeManagementSystem.Models;
 
 namespace EmployeeManagementSystem.Views
 {
-    public partial class Dashboard : ContentPage
+    public partial class Dashboard : ContentPage, INotifyPropertyChanged
     {
-        public ObservableCollection<Employee> Employees { get; set; } = new ObservableCollection<Employee>();
-        public ObservableCollection<Department> Departments { get; set; } = new ObservableCollection<Department>();
+        private ObservableCollection<Employee> _employees;
+        private ObservableCollection<Department> _departments;
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => _employees;
+            set
+            {
+                if (_employees != value)
+                {
+                    _employees = value;
+                    OnPropertyChanged(nameof(Employees));
+                }
+            }
+        }
+
+        public ObservableCollection<Department> Departments
+        {
+            get => _departments;
+            set
+            {
+                if (_departments != value)
+                {
+                    _departments = value;
+                    OnPropertyChanged(nameof(Departments));
+                }
+            }
+        }
 
         public Dashboard()
         {
             InitializeComponent();
-           
-            BindingContext = this;
+            Employees = new ObservableCollection<Employee>();
+            Departments = new ObservableCollection<Department>();
+            BindingContext = this; // Set the page's BindingContext
         }
-
-    
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -24,28 +49,43 @@ namespace EmployeeManagementSystem.Views
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-    
-    private async void OnEmployeeManagementButtonClicked(object sender, EventArgs e)
+
+        private async void OnEmployeeManagementButtonClicked(object sender, EventArgs e)
         {
-            await App.NavigateToPage(new EmployeeManagement()); ;
+            await NavigateToPageAsync(new EmployeeManagement());
         }
 
         private async void OnLeaveRequestsButtonClicked(object sender, EventArgs e)
         {
-            await App.NavigateToPage(new LeaveRequests());
+            await NavigateToPageAsync(new LeaveRequests());
         }
+
         private async void OnAdminSettingsButtonClicked(object sender, EventArgs e)
         {
-            await App.NavigateToPage(new AdminSettings());
+            await NavigateToPageAsync(new AdminSettings());
         }
+
         private async void OnUserSettingsButtonClicked(object sender, EventArgs e)
         {
-            await App.NavigateToPage(new UserSettings());
+            await NavigateToPageAsync(new UserSettings());
         }
 
         private async void OnLogOutButtonClicked(object sender, EventArgs e)
         {
-            await App.NavigateToPage(new LoginView());
+            await NavigateToPageAsync(new LoginView());
+        }
+
+        private async Task NavigateToPageAsync(ContentPage targetPage)
+        {
+            try
+            {
+                // Ensure the navigation is handled centrally
+                await Navigation.PushAsync(targetPage);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Navigation Error", $"Unable to navigate: {ex.Message}", "OK");
+            }
         }
     }
 }
