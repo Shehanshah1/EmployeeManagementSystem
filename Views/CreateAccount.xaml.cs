86a3f8835db5a6d1033a27fc2010c262c84213a4
rondickson <146130094+rondickson@users.noreply.github.com>
2024-11-26 18:28:34 -0600

    Update CreateAccount.xaml.cs

--- Views/CreateAccount.xaml.cs
+++ Views/CreateAccount.xaml.cs
@@ -2,10 +2,20 @@ namespace EmployeeManagementSystem.Views;
 
 public partial class CreateAccount : ContentPage
 {
-	public CreateAccount()
-	{
-		InitializeComponent();
-	}
+    public CreateAccount()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCreateAccountButtonClicked(object sender, EventArgs e)
+    {
+        // Logic for creating account goes here
+        // Example: await CreateAccountAsync();
+
+        // Navigate to Dashboard after creating account
+        await App.NavigateToPage(new Dashboard());
+    }
+
     private async void OnBackButtonClicked(object sender, EventArgs e)
     {
         // Navigate to Dashboard after login
